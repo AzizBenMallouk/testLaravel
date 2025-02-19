b9f0e3799f7cf69990880f13179e4e1623f1f78a
azizbenmallouk <azizbenmallouk4@gmail.com>
2025-02-19 18:13:16 +0100

    te

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,12 +11,23 @@ class CategorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        \DB::table('categories')->insert([
-            'name' => 'Category 1',
-            'slug' => 'category-1',
-            'description' => 'Description for category 1',
-        ]);
+    public function run(){
+    //     $category->name = 'Category 2';
+    //     $category->slug = 'category-2';
+    //     $category->description = 'Description for category 2';
+    //     $category->save();
+    // {
+    //     \DB::table('categories')->insert([
+    //         'name' => 'Category 1',
+    //         'slug' => 'category-1',
+    //         'description' => 'Description for category 1',
+    //     ]);
+
+
+    //     Category::create([
+    //         'name' => 'Category 2',
+    //         'slug' => 'category-2',
+    //         'description' => 'Description for category 2',
+    //     ]);
     }
 }
